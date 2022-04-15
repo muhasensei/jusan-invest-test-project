fccb69445d9e5c23fc2ec1dcef6f5f696788c0d2
muha.io <zhanmuha01@gmail.com>
2022-04-15 10:47:41 +0600

    changed modal message text color

--- src/components/ui-kits/Modal/Modal.jsx
+++ src/components/ui-kits/Modal/Modal.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 const Modal = ({message, showModal, closeModal}) => {
   return <div className={`modal ${showModal && 'show-modal'}`}>
     <div className="modal-content">
-        <h3>{message}</h3>
+        <h3 className='modal-text'>{message}</h3>
         <button className="close-button" onClick={closeModal}>&times;</button>
     </div>
   </div>;
